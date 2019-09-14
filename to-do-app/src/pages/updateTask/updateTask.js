de88bfa1a55581a6748d7a5f8802d00b969cb09c
Isabella Cunha <isabellacunha@id.uff.br>
2019-09-14 14:55:49 -0300

    wip: adicionando pagina de atualização

---
+++ to-do-app/src/pages/updateTask/updateTask.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './updateTask.css';
+
+export default class UpdateTask extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            descricao: 'comprar cebola',
+            status: 'TO DO',
+        }
+    }
+    render() {
+        return(
+            <section className="updateCard">
+                <h1 className="updateTitle">Criar Tarefa</h1>
+                {/* input tem que vir preenchido */}
+                <input className="updateTextBox" type="text" value={this.state.descricao} />
+                <div className="updateStatus__wrapper">
+                    <input className="updateStatus" type="radio" name="teste" value="todo"/>
+                    <label>TO DO</label>
+                    <input className="updateStatus" type="radio" name="teste" value="inprogress"/>
+                    <label>IN PROGRESS</label>
+                    <input className="updateStatus" type="radio" name="teste" value="done"/>
+                    <label>DONE</label>
+                </div>
+                <button className="updateButton">Confirmar</button>
+            </section>
+        )
+    }
+}
