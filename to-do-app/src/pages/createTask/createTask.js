0cf665cb24c14a78976099192f78a5c9aafd53a5
Isabella Cunha <isabellacunha@id.uff.br>
2019-09-14 13:56:22 -0300

    feat: adicionando pagina criar tarefa

---
+++ to-do-app/src/pages/createTask/createTask.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './createTask.css';
+
+export default class CreateTask extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: 'TO DO'
+        }
+    }
+    render() {
+        return(
+            <section className="createCard">
+                <h1 className="createTitle">Criar Tarefa</h1>
+                <span>status: {this.state.status}</span>
+                <input className="createTextBox" type="text" placeholder="Digite aqui sua tarefa"/>
+                <button className="createButton" >Confirmar</button>
+            </section>
+        )
+    }
+}
